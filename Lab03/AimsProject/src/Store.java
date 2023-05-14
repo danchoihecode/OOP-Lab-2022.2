5e2826e324aaa1822f7b3cbce7ed984f52f784b1
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2023-05-14 10:14:12 +0700

    Add Store class

---
+++ Lab03/AimsProject/src/Store.java
@@ -0,0 +1,27 @@
+
+public class Store {
+	private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[100];
+	private int numberOfItem = 0;
+
+	public void addDVD(DigitalVideoDisc disc) {
+		itemsInStore[numberOfItem] = disc;
+		numberOfItem++;
+		System.out.println("The disc has been added to the store");
+	}
+
+	public void removeDVD(DigitalVideoDisc disc) {
+		for (int i = 0; i < numberOfItem; i++) {
+			if (itemsInStore[i] == disc) {
+				for (int j = i; j < numberOfItem - 1; j++) {
+					itemsInStore[j] = itemsInStore[j + 1];
+				}
+				numberOfItem--;
+				itemsInStore[numberOfItem] = null;
+				System.out.println("The disc has been removed from the store");
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ Lab03/AimsProject/src/StoreTest.java
@@ -0,0 +1,20 @@
+
+public class StoreTest {
+	public static void main(String[] args) {
+		// Create a new store
+		Store aStore = new Store();
+
+		// Add items to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		aStore.addDVD(dvd1);
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		aStore.addDVD(dvd2);
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		aStore.addDVD(dvd3);
+
+		// Remove an item
+		aStore.removeDVD(dvd1);
+	}
+}
