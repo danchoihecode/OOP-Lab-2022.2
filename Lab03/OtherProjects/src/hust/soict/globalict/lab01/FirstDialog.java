95f909cfb5cda53a61ccae590eeba80d9808d97a
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2023-05-14 10:40:13 +0700

    Test string construction methods

---
+++ Lab03/OtherProjects/src/hust/soict/globalict/lab01/FirstDialog.java
@@ -0,0 +1,10 @@
+package hust.soict.globalict.lab01;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+		System.exit(0);
+	}
+}
